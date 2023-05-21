5a2578a34199e45fcef9f15f4bd508a0c4a6d5d3
Edinson Florez <edinson.florezx7@gmail.com>
2023-05-20 22:35:45 -0500

    se agregan las funcionalidades de los botone encriptar, descenipta y copiar con js

---
+++ script.js
@@ -0,0 +1,66 @@
+const textArea = document.querySelector(".textarea");
+const mensaje = document.querySelector(".mensaje");
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value= "";
+    mensaje.style.backgroundImage = "none";
+
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value= "";
+    // mensaje.style.backgroundImage = "none";
+
+}
+
+function copiar() {
+    mensaje.select(); // Selecciona el texto dentro del área de texto
+    document.execCommand("copy"); // Copia el texto seleccionado al portapapeles
+    alert("Texto copiado: " + mensaje.value); // Muestra una alerta con el texto copiado
+  }
+  
+
+function encriptar(stringEncriptada){
+
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(i=0 ; i<matrizCodigo.length ; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada
+}
+
+function desencriptar(stringDesencriptada){
+
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(i=0 ; i<matrizCodigo.length ; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada
+}
+
